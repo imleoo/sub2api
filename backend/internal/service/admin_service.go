dc55ed0377a54db2489bf3718e77940ad26ad386
leoobai <imleoo@gmail.com>
2026-04-05 23:26:06 +0800

    fix: correct indentation and import path for httputil package

--- backend/internal/service/admin_service.go
+++ backend/internal/service/admin_service.go
@@ -15,7 +15,7 @@ import (
 	"github.com/Wei-Shaw/sub2api/internal/pkg/httpclient"
 	"github.com/Wei-Shaw/sub2api/internal/pkg/logger"
 	"github.com/Wei-Shaw/sub2api/internal/pkg/pagination"
-github.com/Wei-Shaw/sub2api/internal/util/httputil
+	"github.com/Wei-Shaw/sub2api/internal/util/httputil"
 )
 
 // AdminService interface defines admin management operations
